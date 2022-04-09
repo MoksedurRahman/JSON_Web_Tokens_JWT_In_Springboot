2f464fdc34045d57c8e76029a9df85b3530d9dd2
Md Moksedur Rahman <moksedur.rahman@gmail.com>
2022-04-10 03:42:02 +0600

    init

---
+++ jwt-security/src/main/java/com/datavisionbd/jwt/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.datavisionbd.jwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "Welcome to home controller";
+    }
+}
---
+++ jwt-security/src/test/java/com/datavisionbd/jwt/JwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datavisionbd.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
